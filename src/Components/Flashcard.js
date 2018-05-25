1525753d3d53509eb20f3768e3a1a40599ab0d4a
jasminabasurita <jasminejacquelin@gmail.com>
2018-05-25 15:58:59 -0400

    Styling feels nearly perfect

--- src/Components/Flashcard.js
+++ src/Components/Flashcard.js
@@ -1,32 +1,54 @@
-import React from "react"
+import React, { Component } from "react"
 import ScrollArea from "react-scrollbar"
+import "./Flashcard.css"
 
-const Flashcard = props => (
-  <div className="card">
-    <div className="back side">{props.content.back}</div>
-    <div className="front side">
-      {!!props.content.image && (
-        <div
-          style={{
-            background: `url(${props.content.image}) no-repeat`,
-            backgroundSize: "cover",
-            backgroundPosition: "center"
-          }}
-        />
-      )}
-      {!!props.content.header && <h4>{props.content.header}</h4>}
-      {!!props.content.text && (
-        <ScrollArea
-          speed={0.8}
-          className="area"
-          contentClassName="content"
-          horizontal={false}
-        >
-          <div>{props.content.text}</div>
-        </ScrollArea>
-      )}
-    </div>
-  </div>
-)
+class Flashcard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      flipped: ""
+    }
+  }
+  handleClick = () => {
+    let flipped = "flipped"
+    if (this.state.flipped) {
+      flipped = ""
+    }
+    this.setState({ flipped })
+  }
+  render() {
+    const { props } = this
+    return (
+      <div onClick={this.handleClick} className={`card ${this.state.flipped}`}>
+        <div className="back side">
+          <h2>{props.content.back}</h2>
+        </div>
+        <div className="front side">
+          {!!props.content.image && (
+            <div
+              className="card-image"
+              style={{
+                background: `url(${props.content.image}) no-repeat`,
+                backgroundSize: "cover",
+                backgroundPosition: "center"
+              }}
+            />
+          )}
+          {!!props.content.header && <h2>{props.content.header}</h2>}
+          {!!props.content.text && (
+            <ScrollArea
+              speed={0.8}
+              className="area"
+              contentClassName="content"
+              horizontal={false}
+            >
+              <div>{props.content.text}</div>
+            </ScrollArea>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Flashcard
--- src/Components/FlashcardBlock.js
+++ src/Components/FlashcardBlock.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import axios from "axios"
 import Flashcard from "./Flashcard"
+import "./FlashcardBlock.css"
 
 class FlashcardBlock extends Component {
   constructor(props) {
